a34e6cf56c50a2378d5a4b8348398fa192d9b613
Ravi <99581144+ravigithubshankar@users.noreply.github.com>
2024-06-03 22:32:13 +0530

    Delete detectron2 directory

--- detectron2/checkpoint/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import catalog as _UNUSED  # register the handler
-from .detection_checkpoint import DetectionCheckpointer
-from fvcore.common.checkpoint import Checkpointer, PeriodicCheckpointer
-
-__all__ = ["Checkpointer", "PeriodicCheckpointer", "DetectionCheckpointer"]
